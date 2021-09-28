1f20157fc60b5763bea4b4e20e8b2a1deeac147e
jvblum <jvmell.skch@gmail.com>
2021-09-27 20:35:59 -0400

    add urls_new.ejs

--- express-server.js
+++ express-server.js
@@ -10,25 +10,12 @@ const urlDatabase = {
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.send('Hello there. Add "/urls" to get anywhere.');
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
- });
 
  app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -40,6 +27,9 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
